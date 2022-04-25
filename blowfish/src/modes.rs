2d2c2aa2f8005e1ae106f8ecbc5a439ca53dbccf
retanar <retanarrekotars@gmail.com>
2022-04-25 13:07:48 +0300

    light improvements and error handling

--- blowfish/src/modes.rs
+++ blowfish/src/modes.rs
@@ -1,10 +1,11 @@
-use std::io::{Read, Write};
+use std::io::{Read, Write, Result};
 
 use crate::blowfish::*;
+use crate::consts::{BLOCK_SIZE};
 
 fn enc8(bytes: &[u8], keys: &Keys) -> Vec<u8> {
     let (p1, p2) = encode_block(bytes_to_u32(&bytes[..4]), bytes_to_u32(&bytes[4..]), &keys);
-    let mut v = Vec::with_capacity(8);
+    let mut v = Vec::with_capacity(BLOCK_SIZE);
     v.extend_from_slice(&p1.to_be_bytes());
     v.extend_from_slice(&p2.to_be_bytes());
     return v
@@ -12,7 +13,7 @@ fn enc8(bytes: &[u8], keys: &Keys) -> Vec<u8> {
 
 fn dec8(bytes: &[u8], keys: &Keys) -> Vec<u8> {
     let (p1, p2) = decode_block(bytes_to_u32(&bytes[..4]), bytes_to_u32(&bytes[4..]), &keys);
-    let mut v = Vec::with_capacity(8);
+    let mut v = Vec::with_capacity(BLOCK_SIZE);
     v.extend_from_slice(&p1.to_be_bytes());
     v.extend_from_slice(&p2.to_be_bytes());
     return v
@@ -33,96 +34,100 @@ pub fn unpad_pkcs7(block: &[u8], block_size: usize) -> Vec<u8> {
     } else { block.to_vec() }
 }
 
-pub fn enc_ecb<R, W>(input: &mut R, key: &[u8], out: &mut W)
+pub fn enc_ecb<R, W>(input: &mut R, key: &[u8], out: &mut W) -> Result<()>
 where 
     R: Read,
     W: Write,
 {
     let keys = Keys::new(key);
-    let mut buf = vec![0u8; 8];
+    let mut buf = vec![0u8; BLOCK_SIZE];
     let mut read_len: usize;
 
     loop {
-        read_len = input.read(&mut buf).unwrap();
-        if read_len < 8 {
-            buf = pad_pkcs7(&buf[..read_len], 8);
+        read_len = input.read(&mut buf)?;
+        if read_len < BLOCK_SIZE {
+            buf = pad_pkcs7(&buf[..read_len], BLOCK_SIZE);
         }
         let encoded = enc8(&buf, &keys);
-        out.write(&encoded);
-        if read_len < 8 { break; }
+        out.write(&encoded)?;
+        if read_len < BLOCK_SIZE { break; }
     }
-    out.flush();
+    out.flush()?;
+    Ok(())
 }
 
-pub fn dec_ecb<R, W>(input: &mut R, key: &[u8], out: &mut W)
+pub fn dec_ecb<R, W>(input: &mut R, key: &[u8], out: &mut W) -> Result<()>
 where
     R: Read,
     W: Write,
 {
     let keys = Keys::new(key);
-    let mut buf = vec![0u8; 8];
-    let mut read_len = input.read(&mut buf).unwrap();
+    let mut buf = vec![0u8; BLOCK_SIZE];
+    let mut read_len = input.read(&mut buf)?;
     let mut decoded: Vec<u8>;
 
-    while read_len == 8 {
+    while read_len == BLOCK_SIZE {
         decoded = dec8(&buf, &keys);
-        read_len = input.read(&mut buf).unwrap();
-        if read_len == 0 {
-            decoded = unpad_pkcs7(&decoded, 8);
+        read_len = input.read(&mut buf)?;
+        if read_len < BLOCK_SIZE {
+            decoded = unpad_pkcs7(&decoded, BLOCK_SIZE);
         }
-        out.write(&decoded);
+        out.write(&decoded)?;
     }
-    out.flush();
+    out.flush()?;
+    Ok(())
 }
 
 fn xor(data: &[u8], key: &[u8]) -> Vec<u8> {
     data.iter().zip(key.iter().cycle()).map(|(a, b)| a ^ b).collect()
 }
 
-pub fn enc_cbc<R, W>(input: &mut R, key: &[u8], iv: &[u8], out: &mut W)
+pub fn enc_cbc<R, W>(input: &mut R, key: &[u8], iv: &[u8], out: &mut W) -> Result<()>
 where
     R: Read,
     W: Write,
 {
     let keys = Keys::new(key);
-    let mut buf = vec![0u8; 8];
-    let mut encoded = iv[..8].to_vec();
+    let mut buf = vec![0u8; BLOCK_SIZE];
+    let mut encoded = iv[..BLOCK_SIZE].to_vec();
     let mut read_len: usize;
 
     loop {
-        read_len = input.read(&mut buf).unwrap();
-        if read_len < 8 {
-            buf = pad_pkcs7(&buf[..read_len], 8);
+        read_len = input.read(&mut buf)?;
+        if read_len < BLOCK_SIZE {
+            buf = pad_pkcs7(&buf[..read_len], BLOCK_SIZE);
         }
         buf = xor(&buf, &encoded);
         encoded = enc8(&buf, &keys);
-        out.write(&encoded);
-        if read_len < 8 { break; }
+        out.write(&encoded)?;
+        if read_len < BLOCK_SIZE { break; }
     }
-    out.flush();
+    out.flush()?;
+    Ok(())
 }
 
-pub fn dec_cbc<R, W>(input: &mut R, key: &[u8], iv: &[u8], out: &mut W)
+pub fn dec_cbc<R, W>(input: &mut R, key: &[u8], iv: &[u8], out: &mut W) -> Result<()>
 where
     R: Read,
     W: Write,
 {
     let keys = Keys::new(key);
-    let mut buf = vec![0u8; 8];
-    let mut prev_enc = iv[..8].to_vec();
-    let mut read_len = input.read(&mut buf).unwrap();
+    let mut buf = vec![0u8; BLOCK_SIZE];
+    let mut prev_enc = iv[..BLOCK_SIZE].to_vec();
+    let mut read_len = input.read(&mut buf)?;
 
-    while read_len == 8 {
+    while read_len == BLOCK_SIZE {
         let mut decoded = dec8(&buf, &keys);
         decoded = xor(&decoded, &prev_enc);
         prev_enc.copy_from_slice(&buf);
-        read_len = input.read(&mut buf).unwrap();
-        if read_len < 8 {
-            decoded = unpad_pkcs7(&decoded, 8);
+        read_len = input.read(&mut buf)?;
+        if read_len < BLOCK_SIZE {
+            decoded = unpad_pkcs7(&decoded, BLOCK_SIZE);
         }
-        out.write(&decoded);
+        out.write(&decoded)?;
     }
-    out.flush();
+    out.flush()?;
+    Ok(())
 }
 
 #[allow(non_upper_case_globals)]
@@ -137,11 +142,11 @@ mod tests {
         let crypted: [u8; 16] = [0x24, 0x66, 0xDD, 0x87, 0x8B, 0x96, 0x3C, 0x9D, 0x7D, 0x0C, 0xC6, 0x30, 0xAF, 0xDA, 0x1E, 0xC7];
         let mut actual = Vec::with_capacity(24);
 
-        enc_ecb(&mut plain.clone().as_slice(), &key, &mut actual);
+        enc_ecb(&mut plain.clone().as_slice(), &key, &mut actual).unwrap();
         assert_eq!(&crypted, &actual[..16]);
 
         actual.clear();
-        dec_ecb(&mut crypted.as_slice(), &key, &mut actual);
+        dec_ecb(&mut crypted.as_slice(), &key, &mut actual).unwrap();
         assert_eq!(&plain, &actual[..]);
     }
 
@@ -153,11 +158,11 @@ mod tests {
         let crypted: [u8; 32] = [0x6B, 0x77, 0xB4, 0xD6, 0x30, 0x06, 0xDE, 0xE6, 0x05, 0xB1, 0x56, 0xE2, 0x74, 0x03, 0x97, 0x93, 0x58, 0xDE, 0xB9, 0xE7, 0x15, 0x46, 0x16, 0xD9, 0x59, 0xF1, 0x65, 0x2B, 0xD5, 0xFF, 0x92, 0xCC];
         let mut actual = Vec::with_capacity(32);
 
-        enc_cbc(&mut plain.as_slice(), &key, &iv, &mut actual);
+        enc_cbc(&mut plain.as_slice(), &key, &iv, &mut actual).unwrap();
         assert_eq!(&crypted, &actual[..32]);
 
         actual.clear();
-        dec_cbc(&mut crypted.as_slice(), &key, &iv, &mut actual);
+        dec_cbc(&mut crypted.as_slice(), &key, &iv, &mut actual).unwrap();
         assert_eq!(&plain, &actual.as_slice());
     }
 }
